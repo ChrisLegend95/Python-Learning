2356a8bbfbe3ffd68e1fffd7df082495a3776c85
Chris <52170773+ChrisLegend95@users.noreply.github.com>
2021-12-15 08:11:38 +0100

    Fixed the documentation of this one
    This should clear things up and add some understanding of the "place()" tool in tkinter

--- Place.py
+++ Place.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+#---place() Is really accurate and a good way to design and place diffrent tools as a button or text...ect.
+#--I personal say that "place()" is mostly for a cleaning up after using "pack()", "grid()". But it's free for you to use how you want.
 
 root = tk.Tk()
 root.title("Placing")
@@ -7,10 +9,11 @@ root.resizable(0, 0)
 
 line1 = tk.Label(text="Hello world.\n This is 100,X and 100,Y using Place")
 line1.place(x=100, y=100)
-#----Place(x=?, y=?)--is a solid way to make the placment of labelor button and other widgets to apper where you want them.
+#--Place is use to tell the code what pixle this will go in. (Horizontal and vertical offset in pixels)
 
 button1 = tk.Button(text="x=100, y=50")
 button1.place(x=100, y=50)
+#--Here Button1 is on 100 Horizontal on pixel and 50 on vertical pixel.
 
 button2 = tk.Button(text="x=190, y=50")
 button2.place(x=190, y=50)
